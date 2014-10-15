1e9e6e45d3c8b7fa3c6553fce343a7047a1f0435
Radim Sückr <radim.suckr@gmail.com>
2014-10-15 09:18:32 +0200

    Change of directory for temporary data for testing & little bit of code reformat

--- test/server/file_test.dart
+++ test/server/file_test.dart
@@ -3,16 +3,16 @@ import 'package:cargo/cargo_server.dart';
 
 void main() {
   // First tests!
-  Cargo storage = new Cargo(MODE: CargoMode.FILE, conf: { "path" : "../store/"});
+  Cargo storage = new Cargo(MODE: CargoMode.FILE, conf: {"path": "./"});
 
   storage.start().then((_) {
     test('test basic json storage', () {
-        storage.clear();
-        storage.setItem("data", {"data": "data"});
+      storage.clear();
+      storage.setItem("data", {"data": "data"});
 
-        var data = storage.getItemSync("data");
-        expect(data["data"], "data");
-        expect(storage.length(), 1);
+      var data = storage.getItemSync("data");
+      expect(data["data"], "data");
+      expect(storage.length(), 1);
     });
 
     Map asyncData = {"as": "go"};
@@ -25,4 +25,3 @@ void main() {
     });
   });
 }
-
